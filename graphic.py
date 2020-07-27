b9cce73efeb9bb00e2898993648126256dd44d5a
sbalan7 <sbalan7@outlook.com>
2020-07-27 17:15:33 +0530

    gui images

--- graphic.py
+++ graphic.py
@@ -66,60 +66,21 @@ class tscGUI(QMainWindow):
         pixmap = QPixmap(fname[0])
         self.img.setPixmap(pixmap.scaled(self.img.size()))
         image = Image.open(fname[0])
-        image = image.resize((28, 28))
+        image = image.resize((30, 30))
         image = np.expand_dims(image, axis=0)
         image = np.array(image)
-        self.text3.setText(classes[model.predict_classes([image])[0]+1])
+        self.text3.setText(classes[model.predict_classes([image])[0]])
 
-
-
-classes = { 
-    1:'Speed limit (20km/h)',
-    2:'Speed limit (30km/h)', 
-    3:'Speed limit (50km/h)', 
-    4:'Speed limit (60km/h)', 
-    5:'Speed limit (70km/h)', 
-    6:'Speed limit (80km/h)', 
-    7:'End of speed limit (80km/h)', 
-    8:'Speed limit (100km/h)', 
-    9:'Speed limit (120km/h)', 
-    10:'No passing', 
-    11:'No passing vehicles over 3.5 tons', 
-    12:'Right-of-way at intersection', 
-    13:'Priority road', 
-    14:'Yield', 
-    15:'Stop', 
-    16:'No vehicles', 
-    17:'Vehicles > 3.5 tons prohibited', 
-    18:'No entry', 
-    19:'General caution', 
-    20:'Dangerous curve left', 
-    21:'Dangerous curve right', 
-    22:'Double curve', 
-    23:'Bumpy road', 
-    24:'Slippery road', 
-    25:'Road narrows on the right', 
-    26:'Road work', 
-    27:'Traffic signals', 
-    28:'Pedestrians', 
-    29:'Children crossing', 
-    30:'Bicycles crossing', 
-    31:'Beware of ice/snow',
-    32:'Wild animals crossing', 
-    33:'End speed + passing limits', 
-    34:'Turn right ahead', 
-    35:'Turn left ahead', 
-    36:'Ahead only', 
-    37:'Go straight or right', 
-    38:'Go straight or left', 
-    39:'Keep right', 
-    40:'Keep left', 
-    41:'Roundabout mandatory', 
-    42:'End of no passing', 
-    43:'End no passing vehicle > 3.5 tons' 
-}
+classes = ['20 km/h', '30 km/h', '50 km/h', '60 km/h', '70 km/h', '80 km/h', '80 km/h end', 
+           '100 km/h', '120 km/h', 'No overtaking', 'No overtaking for tracks', 'Crossroad with secondary way', 
+           'Main road', 'Give way', 'Stop', 'Road up', 'Road up for track', 'Brock', 'Other dangerous', 
+           'Turn left', 'Turn right', 'Winding road', 'Hollow road', 'Slippery road', 'Narrowing road', 
+           'Roadwork', 'Traffic light', 'Pedestrian', 'Children', 'Bike', 'Snow', 'Deer', 'End of the limits', 
+           'Only right', 'Only left', 'Only straight', 'Only straight and right', 'Only straight and left', 
+           'Take right', 'Take left', 'Circle crossroad', 'End of overtaking limit', 'End of overtaking limit for track']
 
 app = QApplication(sys.argv)
-model = load_model('traffic_classifier_a.h5')
+model = load_model('traffic_classifier_v.h5')
 gui = tscGUI()
 sys.exit(app.exec_())
+
